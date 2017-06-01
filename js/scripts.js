94c07f001388538ee8f753afbf7c1c8680a0654f
Martha Gonzalez and Patrick Napper <martha@>
2017-05-31 20:26:48 -0700

    add checkboxes, select boxes, radio buttons and color selector.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     var inputNameInput = $("input#inputName").val();
-    var inputDateInput = $("input#inputDate").val();
+    var inputDateInput = $("#Date").val();
     var inputStartTimeInput = $("input#inputStartTime").val();
     var inputEndTimeInput = $("input#inputEndTime").val();
+    var service = $("#service").val();
+    var stylist = $("input:radio[name=stylist]:checked").val();
 
     $(".inputName").text(inputNameInput);
     $(".inputDate").text(inputDateInput);
     $(".inputStartTime").text(inputStartTimeInput);
     $(".inputEndTime").text(inputEndTimeInput);
+    $(".service").text(service);
+    $(".stylist").text(stylist);
 
     $("#appointment").show();
 
